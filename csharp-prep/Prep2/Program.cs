b3c277e49923686cb7342755048e8dec7cbeb7a8
Holden Metzger <holdenmetzger@gmail.com>
2025-10-30 14:17:08 -0600

    Prep Assignment 2 - Grade Calculator

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,58 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int gradeNumber;
+        string letterGrade;
+        string gradeSign = "";
+
+        Console.Write("Please Enter Your Grade as a Number: ");
+        gradeNumber = int.Parse(Console.ReadLine());
+
+        if (gradeNumber >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradeNumber >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradeNumber >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradeNumber >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F"; ;
+        }
+
+
+        if ((gradeNumber % 10) >= 7 && gradeNumber < 90 && gradeNumber >= 60)
+        {
+            gradeSign = "+";
+        }
+        else if ((gradeNumber % 10) < 3 && gradeNumber >= 60)
+        {
+            gradeSign = "-";
+        }
+        
+        Console.WriteLine($"Grade for this Class {letterGrade}{gradeSign}");
+
+        if (gradeNumber >= 70)
+        {
+            Console.WriteLine("Congrats! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Unfortunately you did not pass the class. Better luck next time!");
+        }
+        
     }
 }
