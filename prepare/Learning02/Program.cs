16b828531124b632b7d454578d94c917e22b53aa
Holden Metzger <holdenmetzger@gmail.com>
2025-11-03 21:56:19 -0700

    Complete Abstraction Acitivity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Security.Cryptography.X509Certificates;
+
+
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "GPU Architecture Engineer";
+        job1._company = "Nvidia";
+        job1._startYear = 2018;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Janitor";
+        job2._company = "McDonald's";
+        job2._startYear = 2008;
+        job2._endYear = 2024;
+
+        Resume resume = new Resume();
+        resume._name = "Holden Metzger";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
+
+
     }
+
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,15 @@
+using System.Text.Json.Serialization;
+
+public class Resume
+{
+    public string _name;
+    public List<Job> _jobs = new List<Job>();
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}\nJobs:");
+        foreach (Job job in _jobs)
+        {
+            job.Display();
+        }   
+    }
+}
