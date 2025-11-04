f370bb4999f7d63b7bdaa8d1a9005d247f96a455
Holden Metzger <holdenmetzger@gmail.com>
2025-11-03 17:42:58 -0700

    Completed Step 1

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,26 @@
 using System;
+using System.Security.Cryptography;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number?: ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess?: ");
+        int guess = int.Parse(Console.ReadLine());
+        if (magicNumber == guess)
+        {
+            Console.WriteLine("Correct!");
+        }
+        else if (guess > magicNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("Higher");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,9 +2,13 @@ using System;
 
 class Program
 {
+    static void testValues(int x, float y, double z)
+    {
+        Console.WriteLine($"The values are: {x}, {y}, {z}");
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        /*Console.WriteLine("Hello Sandbox World!");
         string firstName;
         string lastName;
 
@@ -12,8 +16,54 @@ class Program
         firstName = Console.ReadLine(); 
         
         Console.Write("Please enter your last name: ");
-        lastName = Console.ReadLine(); 
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}"); 
+
+        Console.WriteLine(Math.Pow(2, 10));
+
+        int x = 10;
+        Console.WriteLine($"{x}, {++x}, {x++}, {x}");
+
+        for (int i = -1000; i <= 1000; i++)
+        {
+            Console.WriteLine(i * 10);
+        } 
+
+        bool done = false;
+
+        while (!done)
+        {
+            Console.Write("Please Input Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 0 && age <= 125)
+            {
+                done = true;
+                Console.WriteLine($"Super age: {age}");
+            }
+        }
+
+        done = false;
+        do
+        {
+            Console.Write("Please Input Your Age: ");
+            int age = int.Parse(Console.ReadLine());
+            if (age >= 0 && age <= 125)
+            {
+                done = true;
+                Console.WriteLine($"Super age: {age}");
+            }
+        } while(!done); */
+
+        List<int> numbers = new List<int>();
+        numbers.Add(10);
+        numbers.Add(435);
+        numbers.Add(432);
+
+        foreach(int n in numbers)
+        {
+            Console.WriteLine($"The number is {n}");
+        }
         
-        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
 }
