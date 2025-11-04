7444f436ef2d8cf02103aef335a604cf5cbd61b3
Holden Metzger <holdenmetzger@gmail.com>
2025-11-03 18:11:29 -0700

    Completed Stretch 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Security.Cryptography;
 
 class Program
@@ -7,31 +8,37 @@ class Program
     {
         Random rng = new Random();
 
-        int guess;
-        int magicNumber = rng.Next(1, 101);
-        int count = 0;
-
-        do
+        int again = 1;
+        while (again != 0)
         {
-            Console.Write("What is your guess?: ");
-            guess = int.Parse(Console.ReadLine());
+            int guess;
+            int magicNumber = rng.Next(1, 101);
+            int count = 0;
+
+            do
+            {
+                Console.Write("What is your guess?: ");
+                guess = int.Parse(Console.ReadLine());
 
 
-            if (magicNumber == guess)
-            {
-                Console.WriteLine("Correct!");
-            }
-            else if (guess > magicNumber)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("Higher");
-            }
-            count++;
-        } while (guess != magicNumber);
+                if (magicNumber == guess)
+                {
+                    Console.WriteLine("Correct!");
+                }
+                else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else
+                {
+                    Console.WriteLine("Higher");
+                }
+                count++;
+            } while (guess != magicNumber);
 
-        Console.WriteLine($"You guessed {count} times");
+            Console.WriteLine($"You guessed {count} times");
+            Console.Write("Type [1] to Continue, and [0] to exit: ");
+            again = int.Parse(Console.ReadLine());
+        }
     }
 }
