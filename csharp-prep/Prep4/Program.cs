30e00ad2ad1c8e3f74e6d3c549adf823fda66be0
Holden Metzger <holdenmetzger@gmail.com>
2025-11-04 17:30:23 -0700

    Completed Step 1

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<double> list = new List<double>();
+        double sum = 0;
+
+        double input = -1;
+
+        while (input != 0)
+        {
+            Console.Write("Enter Number: ");
+            input = double.Parse(Console.ReadLine());
+            if (input != 0)
+            {
+                list.Add(input);
+            }
+        }
+        foreach (double n in list)
+        {
+            sum += n;
+        }
+        Console.WriteLine($"The total is: {sum}");
     }
 }
