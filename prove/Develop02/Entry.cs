b227921afb6265250ec985c4aa8ea3f8e037ea6e
Holden Metzger <holdenmetzger@gmail.com>
2025-11-10 17:12:33 -0700

    Entry class now grabs date from the system

--- prove/Develop02/Entry.cs
+++ prove/Develop02/Entry.cs
@@ -1,14 +1,15 @@
 class Entry
 {
     Random rng = new Random();
+    DateTime dateTime = DateTime.Now;
     public string _date;
     public string _prompt;
     public string _response;
     List<string> prompts = new List<string>
     {
         "Who was the most interesting person I interacted with today?",
-        "What was the best part of my day",
-        "How did I see the hand of the Lord in my life today",
+        "What was the best part of my day?",
+        "How did I see the hand of the Lord in my life today?",
         "What was the strongest emotion I felt today?",
         "If I had one thing I could do over today, what would it be?"
     };
@@ -17,7 +18,7 @@ class Entry
 
     public void Display()
     {
-        Console.WriteLine($"{_date}\n{_prompt}\n{_response}\n");
+        Console.WriteLine($"\nDate: {_date} - {_prompt}\n{_response}\n");
     }
     public string CreateFileSystemString()
     {
@@ -34,9 +35,8 @@ class Entry
     public void CreateEntry()
     {
         _prompt = prompts[rng.Next(prompts.Count + 1)];
-        Console.Write("Input Today's Date: ");
-        _date = Console.ReadLine();
-        Console.WriteLine($"Here is today's prompt:\n{_prompt}");
+        _date = dateTime.ToShortDateString();
+        Console.Write($"Here is today's prompt:\n{_prompt}\n > ");
         _response = Console.ReadLine();
     }
 }
