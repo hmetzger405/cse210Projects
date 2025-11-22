37e82fe2809e01623e1f3c71f5fb56f0b602080a
Holden Metzger <holdenmetzger@gmail.com>
2025-11-21 20:00:06 -0700

    Menu able to exit

---
+++ prove/Develop04/BaseActivity.cs
@@ -0,0 +1,54 @@
+class BaseActivity
+{
+    private string _welcomeMessage;
+    private string _description;
+    private string _endingMessage;
+    private int _time = -1;
+    private DateTime _startTime;
+    private DateTime _endTime;
+    private List<string> _prompts;
+    private Random rng = new Random();
+
+    public BaseActivity(string welcomeMessage, string description, string endingMessage, List<string> prompts)
+    {
+        _welcomeMessage = welcomeMessage;
+        _description = description;
+        _endingMessage = endingMessage;
+        _prompts = prompts;
+    }
+
+    public void StartActivity()
+    {
+        Console.WriteLine($"{_welcomeMessage}\n\n{_description}\n");
+        GetTimeLength();
+    }
+
+    public void EndActivity()
+    {
+        Console.WriteLine($"{_endingMessage}");
+    }
+
+    public void GetTimeLength()
+    {
+        do
+        {
+            Console.Write("How long, in seconds, would you like your session? ");
+            try
+            {
+                _time = int.Parse(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                Console.WriteLine("Invalid number, please try again");
+                _time = -1;
+            }
+        } while(_time == -1);
+    }
+
+    public void DisplayPrompt(List<string> prompts)
+    {
+        Console.WriteLine(prompts[rng.Next(0, prompts.Count)]);
+    }
+    
+
+}
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,19 +1,32 @@
 using System;
+using System.Collections;
 
 class Program
 {
     static void Main(string[] args)
     {
         Menu menu = new Menu();
+        BaseActivity baseActivity= new BaseActivity("Welcome!", "This is a test", "Good Job", ["How do you feel today", "Think of a time you stood up for someone"]);
         int decision = 0;
 
         while(decision != 4)
         {
+            Console.Clear();
             menu.PrintMenu();
             decision = menu.ProcessMenu();
-            Console.Clear();
+
+        switch (decision)
+        {
+            case 1:
+                baseActivity.StartActivity();
+                baseActivity.EndActivity();
+                break;
+        }
+
         }
 
         Console.WriteLine(decision);
+
+
     }
 }
